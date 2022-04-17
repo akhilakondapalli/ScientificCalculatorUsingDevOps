6ed3eb760d04278ddd322bc0e0a4d0b7cdddf986
akhila <akhila.kondapalli@iiitb.ac.in>
2022-04-17 06:20:01 +0100

    Updated Junit test cases for SQRT function

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -9,16 +9,14 @@ public class CalculatorTest {
     public void squareRootTruePositive() {
         assertEquals("True positive test for Square root of given input", 5.0, calculator.squareRoot(25), DELTA);
         assertEquals("True positive test for Square root of given input", 3.0, calculator.squareRoot(9), DELTA);
-      
+        assertEquals("True positive test for Square root of given input", 0.0, calculator.squareRoot(0), DELTA);
     }
 
     @Test
     public void squareRootFalsePositive() {
-        assertNotEquals("False positive test for Square root of given input", 5.0, calculator.squareRoot(144), DELTA);
-        assertNotEquals("False positive test for Square root of given input", 3.0, calculator.squareRoot(1000), DELTA);
+        assertNotEquals("False positive test for Square root of given input", 4.0, calculator.squareRoot(0), DELTA);
+        assertNotEquals("False positive test for Square root of given input", 3.0, calculator.squareRoot(-100), DELTA);
+        assertNotEquals("False positive test for Square root of given input", 8.0, calculator.squareRoot(144), DELTA);
+        assertNotEquals("False positive test for Square root of given input", 13.0, calculator.squareRoot(1000), DELTA);
     }
-  
-
-
-
 }
